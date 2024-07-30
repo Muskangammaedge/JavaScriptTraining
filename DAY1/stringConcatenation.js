a8e8bb293061fbfb38d03945e6ce680ee12f0912
MuskanTrivedi <macparadise@MacParadises-MacBook-Pro.local>
2024-07-30 14:51:18 +0530

    removed unused file

--- DAY1/stringConcatenation.js
+++
@@ -1,4 +0,0 @@
-let s1 = "Muskan"
-let s2 = "Trivedi"
-let s3 = s1 + " " +s2;
-console.log(s3);
--- DAY3/arrowFunction.js
+++
@@ -1,4 +0,0 @@
-multiply = (a,b) => a*b;
-
-let result = multiply(6,7);
-console.log("Product of two numbers is " + result)
